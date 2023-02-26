2fb109c74758b65d66ac18c06ccf83a9fc72a38c
huangqitie <huangqitie@xiaomi.com>
2023-02-26 15:07:04 +0800

    service-passenger-user注册到nacos

--- service-passenger-user/src/main/java/com/huangqitie/ServicePassengerUserApplication.java
+++ service-passenger-user/src/main/java/com/huangqitie/ServicePassengerUserApplication.java
@@ -3,13 +3,15 @@ package com.huangqitie;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.huangqitie.mapper")
 public class ServicePassengerUserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServicePassengerUserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePassengerUserApplication.class, args);
+    }
 
 }
